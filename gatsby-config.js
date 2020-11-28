0437229ed96e5a9fe66bdf874f74eeb5bca697fc
freddybihozagara <frdb193@gmail.com>
2020-11-28 11:50:30 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Freddy Bihozagara`,
+    title: `Freddy Bihozagara's Blog`,
     author: {
       name: `Freddy Bihozagara`,
       summary: `who lives and works in Edmonton AB, building useful things.`,
