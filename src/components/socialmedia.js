6ed4619af26fc5e83b94d8283d4cb6c34584d75d
freddybihozagara <frdb193@gmail.com>
2020-11-28 18:02:11 -0700

    social media -initial commit

--- src/components/socialmedia.js
+++ src/components/socialmedia.js
@@ -15,9 +15,9 @@ class SocialMedia extends React.Component{
             <table>
                 <tbody>
                     <tr>
-                        <td><img src="./content/assets/facebook_.png" alt=""/></td>
-                        <td><img src="./content/assets/twitter_.png" alt=""/></td>
-                        <td><img src="./content/assets/instagram.png" alt=""/></td>
+                        <td><img src="../content/assets/facebook_.png" alt=""/></td>
+                        <td><img src="../content/assets/twitter_.png" alt=""/></td>
+                        <td><img src="../content/assets/instagram.png" alt=""/></td>
                     </tr>
                 </tbody>
             </table>
