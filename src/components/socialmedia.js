1126d68e5bf6c6ba9a69ef0222e2228610ab8e7c
freddybihozagara <frdb193@gmail.com>
2020-11-28 20:01:49 -0700

    social media -initial commit

--- src/components/socialmedia.js
+++ src/components/socialmedia.js
@@ -21,13 +21,14 @@ class SocialMedia extends React.Component{
                     <tr>
                         
                         <td><u><a href="https://www.facebook.com/freddybihozagara">facebook</a></u> / </td>
-                        <td><u><a href="https://www.twitter.com/freddybihozagara">twitter</a></u> /</td>
+                        <td><u><a href="https://www.twitter.com/Fbihozagara">twitter</a></u> /</td>
                         <td><u><a href="https://www.instagram.com/freddybihozagara">instagram</a></u> /</td>
                     
                     </tr>
                     
                     <tr>
                         <td><img src='http://unsplash.it/200/200?gravity=west' alt=''/></td>
+                        <td><img src='https://twitter.com/FBihozagara/photo' width='100' height='100'/></td>
                         
                     </tr>
                 </tbody>
