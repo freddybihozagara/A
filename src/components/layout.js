6f7af8d6f4a9da3c7288c4f954dbc4d9b6f2dae6
freddybihozagara <frdb193@gmail.com>
2020-11-28 17:55:17 -0700

    social media -initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import SocialMedia from './socialmedia';
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -23,7 +24,9 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
+      <SocialMedia/>
       <main>{children}</main>
+      <SocialMedia/>
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
---
+++ src/components/socialmedia.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SocialMedia extends React.Component{
+
+
+
+
+
+    render(){
+
+        return(
+
+            <div>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td><img src="./content/assets/facebook_.png" alt=""/></td>
+                        <td><img src="./content/assets/twitter_.png" alt=""/></td>
+                        <td><img src="./content/assets/instagram.png" alt=""/></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            </div>
+
+
+
+            
+        )
+    }
+}
+
+
+export default SocialMedia;
